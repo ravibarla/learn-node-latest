f8fff587d71f4afd5ce48b48d241c90c3dbe1d4d
ravi barla <ravibarla1@gmail.com>
2023-09-17 14:22:34 +0530

    common js module

---
+++ arithematic.js
@@ -0,0 +1,12 @@
+function sum(x, y) {
+  return x + y;
+}
+module.exports.sum = (x, y) => {
+  return x + y;
+};
+function div(x, y) {
+  return x / y;
+}
+
+//1. common js module
+//2. ES6 module
---
+++ result.js
@@ -0,0 +1,9 @@
+//call arithematic function ,get result and print result in terminal
+
+//how to import a module
+const arithematicMaodule = require("./arithematic");
+
+//call sum function
+const result = arithematicMaodule.sum(10, 2);
+
+console.log(result);
