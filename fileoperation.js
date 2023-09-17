053e067158b9327d9f26dd585dfe312659397379
ravi barla <ravibarla1@gmail.com>
2023-09-17 16:26:33 +0530

    another way to read data using encoding

--- fileoperation.js
+++ fileoperation.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 //read file content
 
 console.log("starting to read");
-const buffer = fs.readFileSync("./data.txt");
-console.log(buffer.toString());
+const buffer = fs.readFileSync("./data.txt", { encoding: "utf-8" });
+console.log(buffer);
 
 console.log("another operation being performed");
