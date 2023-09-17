971c2f7ed14ee6fc1a150d6fe585680ce54d4fb5
ravi barla <ravibarla1@gmail.com>
2023-09-17 17:09:02 +0530

    path module for cross platform support

--- fileOperatiosnAsync.js
+++ fileOperatiosnAsync.js
@@ -1,8 +1,14 @@
 //1. import module
 const fs = require("fs");
-
+const path = require("path");
 //reading data
-fs.readFile("data.txt", (err, data) => {
+const filePath = path.join("src", "home", "data.txt");
+
+const absoluteFilePath = path.resolve("src", "home", "data.txt");
+console.log(filePath);
+console.log(absoluteFilePath);
+console.log(path.extname(absoluteFilePath));
+fs.readFile(filePath, (err, data) => {
   if (err) {
     console.log(err);
   }
@@ -10,26 +16,26 @@ fs.readFile("data.txt", (err, data) => {
 });
 
 //write data
-fs.writeFile("employee.txt", "new employee", (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("file is written");
-});
+// fs.writeFile("employee.txt", "new employee", (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log("file is written");
+// });
 
-//append data
-fs.appendFile("employee.txt", "\n another employee", (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("file is written");
-});
+// //append data
+// fs.appendFile("employee.txt", "\n another employee", (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log("file is written");
+// });
 
-//delete file
-fs.unlink("employee.txt", (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("file is deleted");
-});
-console.log("there is another application running");
+// //delete file
+// fs.unlink("employee.txt", (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log("file is deleted");
+// });
+// console.log("there is another application running");
